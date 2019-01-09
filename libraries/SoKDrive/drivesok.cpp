10b2f7f56d2d16484ad445bb3e1d80db1e3d3b4c
jvramallo <jvramallo@gmail.com>
2019-01-09 02:24:24 +0100

    Implementada aceleración del drive sok

--- libraries/SoKDrive/drivesok.cpp
+++ libraries/SoKDrive/drivesok.cpp
@@ -1,132 +1,161 @@
 #include "drivesok.h"
 
 
-
 void DriveSoK::begin ()
 {
 	// Initializing Lights
-	for (int i=0; i<4; ++i)
-	{
-		pinMode(LIGHTS_REAR[i], OUTPUT);
-		digitalWrite(LIGHTS_REAR[i], LOW);
-
-		pinMode(LIGHTS_FRONT[i], OUTPUT);
-		digitalWrite(LIGHTS_FRONT[i], LOW);
-	}
-
-	lightState = false;
-
+	turnForeLights(OFF);
+	turnBackLights(OFF);
 
 	// Initializing motors.  The library will allow you to initialize as many motors as you have memory for.  
 	// If you are using functions like forward that take 2 motors as arguements you can either write new functions 
 	// or call the function more than once.
-	motorFront =new Motor(AIN1, AIN2, PWMA, offsetFront, STBY); // FRONT, -255 LEFT, +255 RIGHT
-	motorRear  =new Motor(BIN1, BIN2, PWMB, offsetRear, STBY);  // REAR, +255 FORWARD, -255 BACKWARD
+	foreMotor =new Motor(AIN1, AIN2, PWMA, offsetFront, STBY); // FRONT: -255 LEFT, 	+255 RIGHT
+	backMotor =new Motor(BIN1, BIN2, PWMB, offsetRear,  STBY); // REAR:  +255 FORWARD, -255 BACKWARD
 
-	turn(50);
-	motorRear->brake();
-}
-
-
-
-// Power 1 - 100 (1 LEFT; 50 - STRAIGHT; 100 - RIGHT)
-void DriveSoK::turn (uint8_t power, int duration)
-{
-	constrain(power, 1, 100);
-	motorFront->drive(map(power, 1, 100, -255, 255), duration);
-//	if (power == 50)
-//		motorFront->brake();
-}
-
-// direction 0 - Backward; 1 - Forward
-// Power 0 - 100
-void DriveSoK::go (bool forward, uint8_t power, int duration)
-{
-	power = map (power, 1, 100, 0, 255);
-	motorRear->drive(forward?power:-power, duration);
-}
-
-void DriveSoK::stop ()
-{
-	motorRear->brake();
-	//motorFront->brake();
-}
-
-
-void DriveSoK::lights ()
-{
-	lightState = !lightState;
-
-	for (int i=0; i<4; ++i)
-	{
-		digitalWrite(LIGHTS_REAR[i], lightState);
-		digitalWrite(LIGHTS_FRONT[i], lightState);
-	}
+	foreMotor->brake();
+	backMotor->brake();
 }
 
 
 DriveSoK::~DriveSoK ()
 {
-	if (motorFront != nullptr)
-		delete(motorFront);
+	if (foreMotor != nullptr)
+		delete(foreMotor);
 
-	if (motorRear != nullptr)
-		delete(motorRear);
+	if (backMotor != nullptr)
+		delete(backMotor);
 }
 
-void DriveSoK::lightsGame ()
+
+// power: 	-100 to 100, left, Straight, Right
+// delay:	seconds to delay
+void DriveSoK::turn (int power, int delayMillis)
 {
-	for (int j=1; j<=5; ++j)
-	{
-		// Lights ON
-		for (int i=0; i<4; ++i)
-		{
-			digitalWrite(LIGHTS_FRONT[i], HIGH);
-			digitalWrite(LIGHTS_REAR[i], HIGH);
-		}
+	analogWriteResolution(8);
 
-		delay(250);
+	power = constrain(power, -100, 100);
+	power = map(power, -100, 100, -255, 255); // 255 is for 8 bit analog write resolution
+	foreMotor->drive(power, delayMillis);
 
-		// Lights OFF
-		for (int i=0; i<4; ++i)
-		{
-			digitalWrite(LIGHTS_FRONT[i], LOW);
-			digitalWrite(LIGHTS_REAR[i], LOW);
-		}
-
-		delay(250);
-	}
-
-	if (lightState)
-	{
-		// Lights ON
-		for (int i=0; i<4; ++i)
-		{
-			digitalWrite(LIGHTS_FRONT[i], HIGH);
-			digitalWrite(LIGHTS_REAR[i], HIGH);
-		}
-	}
+//	if (power == 0)
+//		foreMotor->brake();
 }
+
+
+
+// power: 	-100 to +100, Backward, Stop, Forward
+// delay: 	seconds to delay
+void DriveSoK::go (int power, int delayMillis)
+{
+	analogWriteResolution(8);
+
+	if (power > 0)
+		power += _powerOffset;
+	else if (power < 0)
+		power -= _powerOffset;
+	
+	power = constrain(power, -100, 100);
+	power = map (power, -100, 100, -255, 255); // 255 is for 8 bit analog write resolution
+	backMotor->drive(power, delayMillis);
+
+	// If backwards or stopped, turn on back lights
+	if (power <= 0)
+		turnBackLights(ON);
+	else // forward, turn off back lights
+		turnBackLights(OFF);
+
+//	if (power == 0)
+//		foreMotor->brake();
+}
+
+
+void DriveSoK::stop ()
+{
+	backMotor->brake();
+	
+	// We are pulling the brake, so turn on back lights
+	turnBackLights(ON);
+}
+
+
+void DriveSoK::turnForeLights (bool onOff)
+{
+	for (int i=0; i<4; ++i)
+		digitalWrite(FORE_LIGHTS[i], onOff);
+
+	_foreLightState = onOff;
+}
+
+
+void DriveSoK::turnBackLights (bool onOff)
+{
+	for (int i=0; i<4; ++i)
+		digitalWrite(BACK_LIGHTS[i], onOff);
+
+	_backLightState = onOff;
+}
+
+
+void DriveSoK::flashLights (int delayMillis, int numFlashes)
+{
+	for (int i=1; i<=numFlashes; ++i)
+	{
+		turnForeLights(ON);
+		turnBackLights(ON);
+
+		delay(delayMillis);
+
+		turnForeLights(OFF);
+		turnBackLights(OFF);
+
+		delay(delayMillis);
+	}
+
+	// restore previous light state
+	if (_foreLightState)
+		turnForeLights(ON);
+
+	if (_backLightState)
+		turnBackLights(ON);
+}
+
 
 void DriveSoK::powerOn()
 {
 	// Power on Lights
-	if (!lightState)
-		lights();
+	if (!_foreLightState)
+		turnForeLights(ON);
 }
+
 
 void DriveSoK::shutdown ()
 {
 	// Shutdown Motors
-	motorRear->brake();
-	motorFront->brake();
+	backMotor->brake();
+	foreMotor->brake();
 
-	// Shutdown Lights
-	if (lightState)
-		lights();
+	// flash lights for last time & turn them off
+	_foreLightState = OFF;
+	_backLightState = OFF;
+	flashLights();
 
 	ble.writeCommand("DISCONNECT");
 }
+
+
+void DriveSoK::accelerate ()
+{
+
+	_powerOffset += 10;
+}
+
+
+void DriveSoK::decelerate ()
+{
+	_powerOffset -= 10;
+}
+
 
 void driveLoop ()
 {
@@ -140,23 +169,27 @@ void driveLoop ()
 	else if (command.equalsIgnoreCase("apagar"))
 		car.shutdown();
 	else if (command.equalsIgnoreCase("luces"))
-		car.lights();
+		car.turnForeLights(!car.foreLightState());
 	else if (command.equalsIgnoreCase("parar"))
 		car.stop();
 	else if (command.equalsIgnoreCase("adelante"))
-		car.go(FORWARD, 45);
+		car.go(50);
 	else if (command.equalsIgnoreCase("atrás"))
-		car.go(BACKWARD, 40);
+		car.go(-50);
 	else if (command.equalsIgnoreCase("izquierda"))
-		car.turn(1);
+		car.turn(-100);
 	else if (command.equalsIgnoreCase("recto"))
-		car.turn(50);
+		car.turn(0);
 	else if (command.equalsIgnoreCase("derecha"))
 		car.turn(100);
+	else if (command.equalsIgnoreCase("acelerar"))
+		car.accelerate();
+	else if (command.equalsIgnoreCase("decelerar"))
+		car.decelerate();
 	else if (command.equalsIgnoreCase("especial"))
-		car.lightsGame();
+		car.flashLights();
 	else if (command.equalsIgnoreCase("Código secreto"))
-		car.lightsGame();
+		car.flashLights();
 
 
 }
--- libraries/SoKDrive/drivesok.h
+++ libraries/SoKDrive/drivesok.h
@@ -19,6 +19,7 @@ extern DriveSoK car;
 // Pins definitions for all inputs, keep in mind the PWM defines must be on PWM pins
 // the default pins listed are the ones used on the Redbot (ROB-12097) with
 // the exception of STBY which the Redbot controls with a physical switch
+// NOTE! This pin definition is only valid for SoK Zero
 static const int PWMA = D8;
 static const int AIN2 = D7;
 static const int AIN1 = D6;
@@ -27,8 +28,8 @@ static const int BIN1 = D4;
 static const int BIN2 = D3;
 static const int PWMB = D2;
 
-static const int LIGHTS_FRONT[4] = {A1, A0, SCK, MOSI}; // IZQ_A, IZQ_B, DER_A, DER_B
-static const int LIGHTS_REAR[4]  = {D9, D11, D13, D12}; // IZQ_A, IZQ_B, DER_A, DER_B
+static const int FORE_LIGHTS[4] = {A1, A0, SCK, MOSI}; // IZQ_A, IZQ_B, DER_A, DER_B
+static const int BACK_LIGHTS[4]  = {D9, D11, D13, D12}; // IZQ_A, IZQ_B, DER_A, DER_B
 
 // these constants are used to allow you to make your motor configuration 
 // line up with function names like forward.  Value can be 1 or -1
@@ -38,6 +39,9 @@ static const int offsetRear  = -1;
 static const bool FORWARD  = true;
 static const bool BACKWARD = false;
 
+static const bool ON  = true;
+static const bool OFF = false;
+
 class DriveSoK {
 
 public:
@@ -45,19 +49,30 @@ public:
 
 	void begin ();
 
-	void turn (uint8_t power, int duration = 0);
-	void go (bool backForward, uint8_t power, int duration = 0);
+	void turn (int power, int delayMillis = 0);
+	void go   (int power, int delayMillis = 0);
 	void stop ();
-	void lights();
-	void lightsGame();
+	void accelerate ();
+	void decelerate ();
 
-	void powerOn ();
+	void turnForeLights (bool onOff = ON);
+	void turnBackLights (bool onOff = ON);
+	void flashLights 	(int  delayMillis = 250, int numFlashes = 5);
+
+	void powerOn  ();
 	void shutdown ();
 
+	inline bool foreLightState () { return _foreLightState; }
+	inline bool backLightState () { return _backLightState;  }
+
 private:
-	Motor* motorFront = nullptr;
-	Motor* motorRear  = nullptr;
-	bool lightState   = false;
+	Motor* foreMotor = nullptr;
+	Motor* backMotor = nullptr;
+	
+	bool _foreLightState = OFF;
+	bool _backLightState = OFF;
+	
+	int _powerOffset = 0;
 };
 
 
