7ce1a02bfee5f5b1db51386c50f7e269e07f82a6
nekuneko <nekuneko@users.noreply.github.com>
2021-10-21 21:14:25 +0200

    Changes to match Adafruit SAMD Core 1.7.5

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -46,11 +46,6 @@ extern "C"{
 
 // Include Atmel headers
 #include "sam.h"
-
-#ifdef __SAME51J19A__
-#include "same51j19a.h"
-#endif
-
 #include "wiring_constants.h"
 
 #define clockCyclesPerMicrosecond() ( SystemCoreClock / 1000000L )
@@ -155,12 +150,13 @@ void loop( void ) ;
 
 // USB
 #ifdef USE_TINYUSB
-#include "Adafruit_TinyUSB_Core.h"
+  // Needed for declaring Serial
+  #include "Adafruit_USBD_CDC.h"
 #else
-#include "USB/USBDesc.h"
-#include "USB/USBCore.h"
-#include "USB/USBAPI.h"
-#include "USB/USB_host.h"
+  #include "USB/USBDesc.h"
+  #include "USB/USBCore.h"
+  #include "USB/USBAPI.h"
+  #include "USB/USB_host.h"
 #endif
 
 #endif // Arduino_h
